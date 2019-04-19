9944ea05ac7cd6f8820ef36e4f60cebdef7259d1
Adam Ransom <v0id.gmr@gmail.com>
2019-04-19 11:45:26 +0100

    Change how mission counting works
    Now, the mission complete percentage will be the percentage of times
    that mission was completed in a game where that mission was active, NOT
    the percentage of times that mission was completed out of the total
    number of mission completes.

--- src/output/summary/mission_stat_collection.rs
+++ src/output/summary/mission_stat_collection.rs
@@ -6,43 +6,62 @@ use std::collections::HashMap;
 /// A collection for keeping track of how many times each mission was completed.
 #[derive(Default)]
 pub struct MissionStatCollection {
-    total: u32,
+    total: HashMap<&'static str, u32>,
     stats: HashMap<&'static str, u32>,
 }
 
 impl StatCollection for MissionStatCollection {
     fn aggregate(&mut self, replay: &Replay, _: &ArgMatches) {
-        let missions = &replay.header.result_data.completed_missions;
-
-        if missions.contains(&Mission::BugAmbassador) {
-            increment(&mut self.stats, "Bug Ambassador")
-        }
-        if missions.contains(&Mission::ContactDoubleAgent) {
-            increment(&mut self.stats, "Contact Double Agent")
-        }
-        if missions.contains(&Mission::FingerprintAmbassador) {
-            increment(&mut self.stats, "Fingerprint Ambassador")
-        }
-        if missions.contains(&Mission::InspectStatues) {
-            increment(&mut self.stats, "Inspect Statues")
-        }
-        if missions.contains(&Mission::PurloinGuestList) {
-            increment(&mut self.stats, "Purloin Guest List")
-        }
-        if missions.contains(&Mission::SeduceTarget) {
-            increment(&mut self.stats, "Seduce Target")
-        }
-        if missions.contains(&Mission::SwapStatue) {
-            increment(&mut self.stats, "Swap Statue")
-        }
-        if missions.contains(&Mission::TransferMicrofilm) {
-            increment(&mut self.stats, "Transfer Microfilm")
-        }
-
-        self.total += missions.len() as u32;
+        increment_missions(
+            &mut self.stats,
+            &replay.header.result_data.completed_missions,
+        );
+        increment_missions(
+            &mut self.total,
+            &replay.header.result_data.selected_missions,
+        );
     }
 
     fn print(&self) {
-        print_single!(self, "Missions Completed");
+        let mut collection: Vec<_> = self.stats.iter().collect();
+        collection.sort_by(|(_, a), (_, b)| b.cmp(a));
+
+        println!("Missions Completed:");
+        for (name, value) in collection {
+            println!(
+                "    {}: {} ({:.1}%)",
+                name,
+                value,
+                (*value as f32 / self.total[name] as f32) * 100f32
+            );
+        }
+    }
+}
+
+/// Increment a counter of how many times a mission appeared in a particular list.
+fn increment_missions<'a>(mut stats: &mut HashMap<&'a str, u32>, missions: &[Mission]) {
+    if missions.contains(&Mission::BugAmbassador) {
+        increment(&mut stats, "Bug Ambassador")
+    }
+    if missions.contains(&Mission::ContactDoubleAgent) {
+        increment(&mut stats, "Contact Double Agent")
+    }
+    if missions.contains(&Mission::FingerprintAmbassador) {
+        increment(&mut stats, "Fingerprint Ambassador")
+    }
+    if missions.contains(&Mission::InspectStatues) {
+        increment(&mut stats, "Inspect Statues")
+    }
+    if missions.contains(&Mission::PurloinGuestList) {
+        increment(&mut stats, "Purloin Guest List")
+    }
+    if missions.contains(&Mission::SeduceTarget) {
+        increment(&mut stats, "Seduce Target")
+    }
+    if missions.contains(&Mission::SwapStatue) {
+        increment(&mut stats, "Swap Statue")
+    }
+    if missions.contains(&Mission::TransferMicrofilm) {
+        increment(&mut stats, "Transfer Microfilm")
     }
 }
